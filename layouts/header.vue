4dc147dfdb61d02f91e554ff2db03a891dff63c9
BobLamarley <tms.mignon@gmail.com>
2018-09-04 22:15:08 +0200

    first commit

---
+++ layouts/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a href="index.html">
+							<img class="logo" src="img/logo.png" alt="logo">
+							<img class="logo-alt" src="img/logo-alt.png" alt="logo">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Collapse nav button -->
+					<div class="nav-collapse">
+						<span></span>
+					</div>
+					<!-- /Collapse nav button -->
+				</div>
+</template>
+<style>
+</style>
